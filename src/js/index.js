c163e4da63d85200701cfb4ea2c00ab255251b3e
Shirengate <sweetgames551ggg@gmail.com>
2025-06-01 18:15:19 +0300

    add: main banner, about text, work template

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // логика бургер меню
   const burgerBtn = document.querySelector(".burger-menu");
   const mobileMenu = document.querySelector(".mobile-menu");
   const darkLayer = document.querySelector(".dark-layer");
@@ -22,6 +23,33 @@ document.addEventListener("DOMContentLoaded", () => {
     ) {
       mobileMenu.classList.remove("active");
       burgerBtn.classList.remove("active");
+      body.style.overflow = "";
+      darkLayer.style.backgroundColor = "rgba(0, 0, 0, 0)";
+      darkLayer.style.zIndex = "-1";
+    }
+  });
+  // логика полноэкранного фото
+  const fullscreenPhoto = document.querySelector(".fullscreenn-photo");
+  const galeryImages = document.querySelector(".products__showcase");
+  galeryImages.addEventListener("click", (e) => {
+    if (e.target.classList.contains("products__image")) {
+      body.style.overflow = body.style.overflow === "hidden" ? "" : "hidden";
+      darkLayer.style.zIndex = darkLayer.style.zIndex === "10" ? "-1" : "10";
+      darkLayer.style.backgroundColor =
+        darkLayer.style.backgroundColor === "rgba(0, 0, 0, 0.2)"
+          ? "rgba(0, 0, 0, 0)"
+          : "rgba(0, 0, 0, 0.2)";
+      fullscreenPhoto.style.backgroundImage = `url(${e.target.src})`;
+      fullscreenPhoto.classList.toggle("active");
+    }
+  });
+  document.addEventListener("click", (e) => {
+    if (
+      !fullscreenPhoto.contains(e.target) &&
+      !galeryImages.contains(e.target) &&
+      fullscreenPhoto.classList.contains("active")
+    ) {
+      fullscreenPhoto.classList.remove("active");
       body.style.overflow = "";
       darkLayer.style.backgroundColor = "rgba(0, 0, 0, 0)";
       darkLayer.style.zIndex = "-1";
